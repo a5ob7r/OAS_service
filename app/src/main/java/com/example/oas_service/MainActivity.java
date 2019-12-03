aa3d0f7f50cf9a7cf5cee5647463a36c2ff30daa
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2019-12-03 20:08:50 +0900

    Add a starting service process

--- app/src/main/java/com/example/oas_service/MainActivity.java
+++ app/src/main/java/com/example/oas_service/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.oas_service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    IFizzBuzzService fbService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent intent = new Intent(this, FizzBuzzService.class);
+        intent.setAction(IFizzBuzzService.class.getName());
+        bindService(intent, fbConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(fbConnection);
+    }
+
+    private ServiceConnection fbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            fbService = IFizzBuzzService.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            fbService = null;
+        }
+    };
 }
